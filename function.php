e168bbe77e19de852fc657f2c1c443b77a301a30
cutnasywaymn <80031100+cutnasywaymn@users.noreply.github.com>
2021-05-27 11:22:14 +0700

    menambahkan fungsi untuk aksi keluar

--- function.php
+++ function.php
@@ -162,4 +162,74 @@ if(isset($_POST['updatebarangmasuk'])){
 
 }
 
+
+//mengubah data barang keluar 
+
+if(isset($_POST['updatebarangkeluar'])){
+	$idb = $_POST['idb'];
+	$idk = $_POST['idk'];
+	$penerima = $_POST['penerima'];
+	$qty = $_POST['qty'];
+
+	$lihatstock = mysqli_query($conn, "select * from stock where idbarang='$idb'");
+	$stocknya = mysqli_fetch_array($lihatstock);
+	$stockskrg = $stocknya['stock'];
+
+	$qtyskrg = mysqli_query($conn, "select * from keluar where idkeluar='$idk'");
+	$qtynya = mysqli_fetch_array($qtyskrg);
+	$qtyskrg = $qtynya['qty'];
+
+	if($qty>$qtyskrg){
+		$selisih  = $qty-$qtyskrg;
+		$kurangin = $stockskrg-$selisih;
+		$kurangistocknya = mysqli_query($conn, "update stock set stock='$kurangin' where idbarang='$idb'");
+		$updatenya = mysqli_query($conn, "update keluar set qty='$qty' , penerima='$deskripsi' where idkeluar='$idk'");
+			if($kurangistocknya&&$updatenya){
+				header('location:keluar.php');
+			} else {
+			    echo 'Gagal';
+			    header('location:keluar.php');
+		    }
+		} else {
+			$selisih  = $qtyskrg-$qty;
+			$kurangin = $stockskrg+$selisih;
+			$kurangistocknya = mysqli_query($conn, "update stock set stock='$kurangin' where idbarang='$idb'");
+			$updatenya = mysqli_query($conn, "update keluar set qty='$qty' , penerima='$penerima' where idkeluar='$idk'");
+				if($kurangistocknya&&$updatenya){
+					header('location:keluar.php');
+				} else {
+				    echo 'Gagal';
+				    header('location:keluar.php');
+			    }
+
+
+		}
+
+
+		//menghapus data barang keluar
+ 
+		if(isset($_POST['hapusbarangkeluar'])){
+			$idb = $_POST['idb'];
+			$qty = $_POST['qty'];
+			$idk = $_POST['idk'];
+
+			$getdatastock = mysqli_query($conn, "select * from stock where idbarang='$idb'");
+			$data = mysqli_fetch_array($getdatastock);
+			$stok = $data['stock'];
+
+			$selisih = $stok+$qty;
+
+
+			$update = mysqli_query($conn, "update stock set stock='$selisih' , where idbarang='$idb'");
+			$hapusdata = mysqli_query($conn, "delete from keluar where idkeluar='$idk'");
+
+			if($update&&$hapusdata){
+				header('location:keluar.php');
+			}else {
+				header('location:keluar.php');
+			}
+		}
+
+}
+
 ?>
